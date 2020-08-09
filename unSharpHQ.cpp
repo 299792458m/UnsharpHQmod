8043ed87c4f8533bde3056ff3f21289638f701ed
299792458m <35194929+299792458m@users.noreply.github.com>
2020-08-09 12:31:07 +0900

    一部のHBD、YUV対応追加 最適化未

--- unSharpHQ.cpp
+++ unSharpHQ.cpp
@@ -34,33 +34,140 @@
 
 #include "utiles.h"
 
+template<int bits_per_pixel>
+void FieldUnSharpYHBD(unsigned char* dstp0, const unsigned char* srcp, int rowsize, int height, int src_pitch, int dst_pitch,
+	float thres, float MIN, float MAX, bool show)
+{
+	constexpr int max_pixel_size = (1 << bits_per_pixel);
+	constexpr int max_pixel_value = max_pixel_size - 1;
+	constexpr int cnvsft8toHBD = bits_per_pixel - 8;
+	//__declspec(align(16)) int32_t funcion[max_pixel_size];
+	int32_t* funcion= (int32_t*)_aligned_malloc(max_pixel_size * sizeof(int32_t), 16);	//サイズが大きい(16bitでは256kBになる)ためヒープへ
+
+	//if (show) {
+	//	MIN = -4;
+	//	MAX = 60;
+	//}
+
+	int ocho = 8;	//1<<PRECISIONだが、意味不明 不要では？
+
+	//MIN = -(SMOOTH+1.0)で、MAX = SHAPSTR+1.0になってる
+
+	double giro, atanMIN, atanMAX, A, S;
+	float fmin, fmax;	//MIN/MAXは1ずつ拡張された値が渡される→呼び出し元を変更して拡張しないようにした 必要に応じて↓で調整
+
+
+	//i==giroで1になるようにmode1をオフセットしてそれっぽくしてるが、形はi=giroでatan(0)の形になってる(smooth側があまり考慮されてない)
+	fmin = MIN * ocho / 4 - 0.99999f;	//-1.0にすると演算精度上-1.0001とかになるので
+	fmax = MAX * ocho / 4;
+
+	giro = thres;
+	atanMIN = atan((0 - giro) / curva);			//0なので括弧でくくっても同じだけど
+	atanMAX = atan((255 - giro) / curva);		//funcionが(i-giro)/curvであることを考えると、ここも(i-giro)が正しい
+	A = (fmax - fmin) / (atanMAX - atanMIN);		//(atanMIN,atanMAX)→(MIN,MAX)の傾き
+	S = fmin - A * atanMIN - 1.0;					//(atanMIN,atanMAX)→(MIN,MAX)のオフセット
+
+	for (int ii = 0; ii < bits_per_pixel; ii++) {				//i=giroでfuncionが1になるように繰り返し演算 精度はよく知らんがもし二次収束するならこんなもん
+		giro = thres - tan(-S / A) * curva;
+		atanMIN = atan((0 - giro) / curva);
+		atanMAX = atan((255 - giro) / curva);
+		A = (fmax - fmin) / (atanMAX - atanMIN);
+		S = fmin - A * atanMIN - 1.0;
+	}
+	for (int i = 0; i < max_pixel_size; i++) {
+		funcion[i] = (__int32)(A * atan(((double)i/(1<< cnvsft8toHBD) - giro) / curva) + S + 1.0);	//ここでHBD換算する
+		//test[i]  = ( A*atan( (i -giro) / curva  ) + S +1.0);
+	}
+
+
+	const uint16_t* row0 = (uint16_t*)srcp;		//これが安全かどうかは知らない・・・
+	const uint16_t* row1 = (uint16_t*)(srcp + src_pitch);
+	const uint16_t* row2 = (uint16_t*)(srcp + src_pitch * 2);
+	const uint16_t* row3 = (uint16_t*)(srcp + src_pitch * 3);
+	const uint16_t* row4 = (uint16_t*)(srcp + src_pitch * 4);
+
+	uint16_t* dstp = (uint16_t*)dstp0;	//16bitへキャスト
+
+	memcpy((BYTE*)dstp, (BYTE*)row0, rowsize);		//1,2行目はソースをコピー
+	dstp = dstp + dst_pitch / sizeof(uint16_t);		//pitchはbyte単位なので
+	memcpy((BYTE*)dstp, (BYTE*)row1, rowsize);
+	dstp = dstp + dst_pitch / sizeof(uint16_t);
+
+	int width = rowsize / sizeof(uint16_t);
+	int w;
+	for (int h = 2; h < height - 2; h++) {
+		{
+			dstp[0] = row2[0];			//1,2列目はソースをコピー
+			dstp[1] = row2[1];
+			for (w = 2; w < width - 2; w++) {
+				int avrg, diff, A, B;
+
+				avrg = row0[w]
+					+ row1[w - 1] + row1[w + 1]
+					+ row2[w - 2] + row2[w + 2]
+					+ row3[w - 1] + row3[w + 1]
+					+ row4[w];
+				avrg = avrg / 8;
+
+				diff = abs(avrg - row0[w])
+					+ abs(avrg - row1[w - 1]) + abs(avrg - row1[w + 1])
+					+ abs(avrg - row2[w - 2]) + abs(avrg - row2[w + 2])
+					+ abs(avrg - row3[w - 1]) + abs(avrg - row3[w + 1])
+					+ abs(avrg - row4[w]);			 //asmではabsを2つの大きい方-小さい方でやってる
+				diff = min(max(diff, 0), max_pixel_value);
+
+				B = funcion[diff];
+				A = B + ocho;		// (1+B/ocho)*ocho
+
+				dstp[w] = min(max((row2[w] * A - avrg * B) / ocho, 0), max_pixel_value);
+			}
+			dstp[width - 2] = row2[width - 2];			//last-1,last列はソースをコピー
+			dstp[width - 1] = row2[width - 1];
+		}
+
+		row0 = row1;
+		row1 = row2;
+		row2 = row3;
+		row3 = row4;
+		row4 += src_pitch/ sizeof(uint16_t);
+
+		dstp += dst_pitch/ sizeof(uint16_t);
+	}
+	memcpy((BYTE*)dstp, (BYTE*)row2, rowsize);			//last-1,last行目はソースをコピー
+	dstp = dstp + dst_pitch / sizeof(uint16_t);
+	memcpy((BYTE*)dstp, (BYTE*)row3, rowsize);
+	dstp = dstp + dst_pitch / sizeof(uint16_t);
+
+	_aligned_free(funcion);
+}
+
 class UnsharpHQ : public GenericVideoFilter {   
 	float MIN,MAX;
 	int thres;
 	bool show;
 	int mode;
-	bool lsb_in_flag, lsb_out_flag;
 
 public:
-	UnsharpHQ(PClip _child,int , float ,float ,bool ,int, bool,bool,IScriptEnvironment* env);
+	UnsharpHQ(PClip _child,int , float ,float ,bool ,int,IScriptEnvironment* env);
 	~UnsharpHQ();
 	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 };
 
 
 
-UnsharpHQ::UnsharpHQ(PClip _child,int _thres,float _MAX,float _MIN,bool _show,int _mode, bool _lsb_in_flag, bool _lsb_out_flag, IScriptEnvironment* env) :
+UnsharpHQ::UnsharpHQ(PClip _child,int _thres,float _MAX,float _MIN,bool _show,int _mode, IScriptEnvironment* env) :
 GenericVideoFilter(_child) {
 	thres = _thres;
-	MIN = _MIN;
-	MAX = _MAX;
+	MIN = _MIN;	//SMOOTH
+	MAX = _MAX;	//SHARPSTR
 	show = _show;
 	mode = _mode;
-	lsb_in_flag = _lsb_in_flag;
-	lsb_out_flag = _lsb_out_flag;
 		
-	if( !vi.IsYV12() )
-		env->ThrowError("UnSharpHQ supports YV12 only");
+	if( !vi.IsYUV() || !vi.IsPlanar() )
+		env->ThrowError("UnSharpHQ: supports only planar YUV(YV12,YUV420..)");	//αチャンネルもコピーしてないのでYUVAもダメ
+
+	if (vi.BitsPerComponent() > 16)
+		env->ThrowError("UnsharpHQ: supports only 8-16bit integer input. not float!");
 
 	if( thres<0 || thres>99 || MIN<0 || MIN>4 || MAX<0 || MAX>99)
 		env->ThrowError("UnsharpHQ(THRESHOLD=20, SHARPSTR=4.0 , SMOOTH=0.5 , SHOW=FALSE)  \n "
@@ -70,7 +177,6 @@ GenericVideoFilter(_child) {
 		"     SMOOTH [ 0.0 ,  4.0]\n"
 	);
 	if( mode >=2 ) env->ThrowError("MODE is 0 to 1");
-
 }
 
 UnsharpHQ::~UnsharpHQ() {}
@@ -80,42 +186,93 @@ PVideoFrame __stdcall UnsharpHQ::GetFrame(int n, IScriptEnvironment* env) {
 
 	PVideoFrame src = child->GetFrame(n, env);
 	PVideoFrame dst = env->NewVideoFrame(vi);
+	int bits_per_pixel = vi.BitsPerComponent();
 
-	if (vi.IsYV12()) {
-		const unsigned char* srcp = src->GetReadPtr(PLANAR_Y);
+	if (vi.IsYUV()) {
+		if (bits_per_pixel == 8) {
+			const uint8_t* srcp = src->GetReadPtr(PLANAR_Y);
 
-		unsigned char * dstp = dst->GetWritePtr(PLANAR_Y);
-		int src_pitch = src->GetPitch(PLANAR_Y);
-		int dst_pitch = dst->GetPitch(PLANAR_Y);
-		int rowsize = dst->GetRowSize(PLANAR_Y);  
-		int height = dst->GetHeight(PLANAR_Y);
+			uint8_t* dstp = dst->GetWritePtr(PLANAR_Y);
+			int src_pitch = src->GetPitch(PLANAR_Y);
+			int dst_pitch = dst->GetPitch(PLANAR_Y);
+			int rowsize = dst->GetRowSize(PLANAR_Y);
+			int height = dst->GetHeight(PLANAR_Y);
 
-		FieldUnSharpY(dstp, srcp, rowsize,height,src_pitch,dst_pitch,(float)thres,-MIN,MAX,show,mode);
-	
-		//- - - - - - - - U - - - - - - - - - -  
-		
-		srcp = src->GetReadPtr(PLANAR_U);
-		dstp = dst->GetWritePtr(PLANAR_U);
-		src_pitch = src->GetPitch(PLANAR_U);
-		dst_pitch = dst->GetPitch(PLANAR_U);
-		rowsize = dst->GetRowSize(PLANAR_U);  
-		height = dst->GetHeight(PLANAR_U);
-		
-		if (!show)
-			Fieldcopy(dstp, srcp, rowsize,height, dst_pitch, src_pitch);
-		else
-			FieldNull(dstp,rowsize,height, dst_pitch,VERDE1);
+			FieldUnSharpY(dstp, srcp, rowsize, height, src_pitch, dst_pitch, (float)thres, -MIN, MAX, show, mode);
+
+			//- - - - - - - - U - - - - - - - - - -  
+
+			srcp = src->GetReadPtr(PLANAR_U);
+			dstp = dst->GetWritePtr(PLANAR_U);
+			src_pitch = src->GetPitch(PLANAR_U);
+			dst_pitch = dst->GetPitch(PLANAR_U);
+			rowsize = dst->GetRowSize(PLANAR_U);
+			height = dst->GetHeight(PLANAR_U);
+
+			if (!show)
+				Fieldcopy(dstp, srcp, rowsize, height, dst_pitch, src_pitch);
+			else
+				FieldNull(dstp, rowsize, height, dst_pitch, VERDE1);
+
+			//- - - - - - - - V - - - - - - - - - -  
+			srcp = src->GetReadPtr(PLANAR_V);
+			dstp = dst->GetWritePtr(PLANAR_V);
+			src_pitch = src->GetPitch(PLANAR_V);
+			dst_pitch = dst->GetPitch(PLANAR_V);
+			rowsize = dst->GetRowSize(PLANAR_V);
+			height = dst->GetHeight(PLANAR_V);
+
+			if (!show)
+				Fieldcopy(dstp, srcp, rowsize, height, dst_pitch, src_pitch);
+			else
+				FieldNull(dstp, rowsize, height, dst_pitch, VERDE2);
+
+		}
+		else {	//HBD
+			const uint8_t* srcp = src->GetReadPtr(PLANAR_Y);
+			uint8_t* dstp = dst->GetWritePtr(PLANAR_Y);
+			int src_pitch = src->GetPitch(PLANAR_Y);
+			int dst_pitch = dst->GetPitch(PLANAR_Y);	//次の行までのbyte数(パディング含む)
+			int rowsize = dst->GetRowSize(PLANAR_Y);	//1行のbyte数
+			int height = dst->GetHeight(PLANAR_Y);		//何列あるか(pixel数)
+
+			switch (bits_per_pixel) {
+			case 10:	FieldUnSharpYHBD<10>(dstp, srcp, rowsize, height, src_pitch, dst_pitch, (float)thres, -MIN, MAX, show); break;	//modeは削除
+			case 12:	FieldUnSharpYHBD<12>(dstp, srcp, rowsize, height, src_pitch, dst_pitch, (float)thres, -MIN, MAX, show); break;
+			case 14:	FieldUnSharpYHBD<14>(dstp, srcp, rowsize, height, src_pitch, dst_pitch, (float)thres, -MIN, MAX, show); break;
+			case 16:
+			default:	FieldUnSharpYHBD<16>(dstp, srcp, rowsize, height, src_pitch, dst_pitch, (float)thres, -MIN, MAX, show); break;
+			}
 			
-		//- - - - - - - - V - - - - - - - - - -  
-		srcp = src->GetReadPtr(PLANAR_V);
-		dstp = dst->GetWritePtr(PLANAR_V);
+			//- - - - - - - - U - - - - - - - - - -  
 
-		if (!show)
-			Fieldcopy(dstp, srcp, rowsize,height, dst_pitch, src_pitch);
-		else
-			FieldNull(dstp,rowsize,height, dst_pitch,VERDE2);
+			srcp = src->GetReadPtr(PLANAR_U);
+			dstp = dst->GetWritePtr(PLANAR_U);
+			src_pitch = src->GetPitch(PLANAR_U);
+			dst_pitch = dst->GetPitch(PLANAR_U);
+			rowsize = dst->GetRowSize(PLANAR_U);
+			height = dst->GetHeight(PLANAR_U);
 
-	}	
+			if (!show)
+				Fieldcopy(dstp, srcp, rowsize, height, dst_pitch, src_pitch);
+			else
+				FieldNull(dstp, rowsize, height, dst_pitch, VERDE1);
+
+			//- - - - - - - - V - - - - - - - - - -  
+			srcp = src->GetReadPtr(PLANAR_V);
+			dstp = dst->GetWritePtr(PLANAR_V);
+			src_pitch = src->GetPitch(PLANAR_V);
+			dst_pitch = dst->GetPitch(PLANAR_V);
+			rowsize = dst->GetRowSize(PLANAR_V);
+			height = dst->GetHeight(PLANAR_V);
+
+			if (!show)
+				Fieldcopy(dstp, srcp, rowsize, height, dst_pitch, src_pitch);
+			else
+				FieldNull(dstp, rowsize, height, dst_pitch, VERDE2);
+
+		}
+	}
 	return dst;
 }
 
@@ -128,8 +285,6 @@ AVSValue __cdecl Create_UnsharpHQ(AVSValue args, void* user_data, IScriptEnviron
 		,(float)args[3].AsFloat(smoot_str)
 		,args[4].AsBool(false)
 		,args[5].AsInt(0)
-		,args[6].AsBool(false)
-		,args[7].AsBool(false)
 	, env);
 }
 
--- utiles.h
+++ utiles.h
@@ -28,10 +28,6 @@ void FieldUnSharpY(unsigned char* dstp,const unsigned char* srcp
 				  ,int rowsize,int height,int src_pitch,int dst_pitch,
 				  float thres,float MIN,float MAX,bool show,int mode)
 {
-	if (show){
-		MIN=-4;
-		MAX=60;
-	}
 
 	//__int16 ocho = (__int16) pow( (float)2, (float)PRECISION);
 	__int16 ocho = (1<<PRECISION);
@@ -88,7 +84,7 @@ void FieldUnSharpY(unsigned char* dstp,const unsigned char* srcp
 	//		//test[i]  = ( A*atan( (i -giro) / curva  ) + S );
 	//	}
 	//}
-	else if (mode==1){	//i==giroで1になるようにmode1をオフセットしてそれっぽくしてるが、形はi=giroでatan(0)の形になってる(smooth側があまり考慮されてない)
+	else{	//i==giroで1になるようにmode1をオフセットしてそれっぽくしてるが、形はi=giroでatan(0)の形になってる(smooth側があまり考慮されてない)
 		fmin=MIN*ocho/4-0.99999f;	//-1.0にすると演算精度上-1.0001とかになるので
 		fmax=MAX*ocho/4;
 
@@ -120,277 +116,278 @@ void FieldUnSharpY(unsigned char* dstp,const unsigned char* srcp
 	const unsigned char* row4=srcp+4*src_pitch;
 	
 	memcpy( (BYTE*) dstp, (BYTE*) row0, rowsize);			//1,2行目はソースをコピー
-	memcpy( (BYTE*) dstp+dst_pitch, (BYTE*) row1, rowsize);
-	
-	dstp=dstp+2*dst_pitch;
+	dstp = dstp + dst_pitch;
+	memcpy( (BYTE*) dstp, (BYTE*) row1, rowsize);
+	dstp = dstp + dst_pitch;
+
 	int w;
 	uc AVGs[8];
 	uc DIFs[8];
 
 	for (int h=2; h < height-2; h++){
 #ifdef _WIN64	//最適化はしてない
-		for (w=2;w<=rowsize-2;w++){
-			int avrg,diff,A,B;
+		dstp[0] = row2[0];			//1,2列目はソースをコピー
+		dstp[1] = row2[1];
+		for (w = 2; w < rowsize - 2; w++) {
+			int avrg, diff, A, B;
 
-			avrg = row0[w]+row4[w];
-			if (w < rowsize-1) avrg += row1[w+1]+row2[w+2]+row3[w+1];
-			if (w>2)           avrg += row1[w-1]+row2[w-2]+row3[w-1];
-			avrg = (w<2) ? avrg/5 : (w>rowsize-1) ? avrg/5 : avrg/8;
+			avrg = row0[w] + row4[w];
+			avrg += row1[w + 1] + row2[w + 2] + row3[w + 1];
+			avrg += row1[w - 1] + row2[w - 2] + row3[w - 1];
+			avrg = avrg / 8;
 
-			diff = abs(avrg-row0[w]) + abs(avrg-row4[w]); //asmではabsを2つの大きい方-小さい方でやってる
-			if (w < rowsize-1) diff += abs(avrg-row1[w+1]) + abs(avrg-row2[w+2]) + abs(avrg-row3[w+1]);
-			if (w > 2)         diff += abs(avrg-row1[w-1]) + abs(avrg-row2[w-2]) + abs(avrg-row3[w-1]);
-			diff = min(max(diff,0),255);
+			diff = abs(avrg - row0[w]) + abs(avrg - row4[w]); //asmではabsを2つの大きい方-小さい方でやってる
+			diff += abs(avrg - row1[w + 1]) + abs(avrg - row2[w + 2]) + abs(avrg - row3[w + 1]);
+			diff += abs(avrg - row1[w - 1]) + abs(avrg - row2[w - 2]) + abs(avrg - row3[w - 1]);
+			diff = min(max(diff, 0), 255);
 
 			B = funcion[diff];
 			A = B + ocho;		// (1+B/ocho)*ocho
 
-			dstp[w] =  min(max( (row2[w] * A -avrg * B) / ocho, 0),255);
+			dstp[w] = min(max((row2[w] * A - avrg * B) / ocho, 0), 255);
 		}
-		dstp[0]=row2[0];			//1,2列目はソースをコピー
-		dstp[1]=row2[1];
-		dstp[rowsize-2]=row2[rowsize-2];			//last-1,last列はソースをコピー
-		dstp[rowsize-1]=row2[rowsize-1];
-
+		dstp[rowsize - 2] = row2[rowsize - 2];			//last-1,last列はソースをコピー
+		dstp[rowsize - 1] = row2[rowsize - 1];
 #else
 		//0, 1 , height-1 , height, los copio
-		dstp[0]=row2[0];			//1,2列目はソースをコピー
-		dstp[1]=row2[1];
-		for ( w=2; w < rowsize-2  ; w+=8){
+		dstp[0] = row2[0];			//1,2列目はソースをコピー
+		dstp[1] = row2[1];
+		for (w = 2; w < rowsize - 2; w += 8) {
 
-		//		- - - - -		- - o - -
-		//		- o o o -		- o - o -
-		//		- o X o -	=)	o - X - o
-		//		- o o o -		- o - o -
-		//		- - - - -		- - o - -
+			//		- - - - -		- - o - -
+			//		- o o o -		- o - o -
+			//		- o X o -	=)	o - X - o
+			//		- o o o -		- o - o -
+			//		- - - - -		- - o - -
 
-		//		- - - - -		- - i - -	0
-		//		- a b c -		- j - k -	1
-		//		- d X e -	=)	l - X - m	2
-		//		- f g h -		- n - o -	3
-		//		- - - - -		- - p - -	4
+			//		- - - - -		- - i - -	0
+			//		- a b c -		- j - k -	1
+			//		- d X e -	=)	l - X - m	2
+			//		- f g h -		- n - o -	3
+			//		- - - - -		- - p - -	4
 
-			__asm{
-			// AVG -- 33
-				pxor xmm0 , xmm0;
-				mov eax , row1;
-				mov ebx , w;
-				mov ecx , row3;
-				
-				movq xmm1 , qword ptr[eax+ebx-1];	//j[0-7]	四隅
-				movq xmm2 , qword ptr[eax+ebx+1];	//k
-				movq xmm3 , qword ptr[ecx+ebx-1];	//n
-				movq xmm4 , qword ptr[ecx+ebx+1];	//o
-				punpcklbw xmm1 , xmm0;		//byte->word
-				punpcklbw xmm2 , xmm0;
-				punpcklbw xmm3 , xmm0;
-				punpcklbw xmm4 , xmm0;
+			__asm {
+				// AVG -- 33
+				pxor xmm0, xmm0;
+				mov eax, row1;
+				mov ebx, w;
+				mov ecx, row3;
 
-				paddw xmm1 , xmm2;					//j+k+n+o
-				paddw xmm1 , xmm3;
-				paddw xmm1 , xmm4;
+				movq xmm1, qword ptr[eax + ebx - 1];	//j[0-7]	四隅
+				movq xmm2, qword ptr[eax + ebx + 1];	//k
+				movq xmm3, qword ptr[ecx + ebx - 1];	//n
+				movq xmm4, qword ptr[ecx + ebx + 1];	//o
+				punpcklbw xmm1, xmm0;		//byte->word
+				punpcklbw xmm2, xmm0;
+				punpcklbw xmm3, xmm0;
+				punpcklbw xmm4, xmm0;
 
-				mov eax , row0;
-				mov ecx , row4;
-				mov edx , row2;
-				
-				movq xmm2 , qword ptr[eax+ebx];		//i
-				movq xmm3 , qword ptr[ecx+ebx];		//p
-				movq xmm4 , qword ptr[edx+ebx+2];	//l
-				movq xmm5 , qword ptr[edx+ebx-2];	//m
-				punpcklbw xmm2 , xmm0;		//byte->word
-				punpcklbw xmm3 , xmm0;
-				punpcklbw xmm4 , xmm0;
-				punpcklbw xmm5 , xmm0;
+				paddw xmm1, xmm2;					//j+k+n+o
+				paddw xmm1, xmm3;
+				paddw xmm1, xmm4;
 
-				paddw xmm1 , xmm2;					//j+k+n+o +i+p+l+m
-				paddw xmm1 , xmm3;
-				paddw xmm1 , xmm4;
-				paddw xmm1 , xmm5;
+				mov eax, row0;
+				mov ecx, row4;
+				mov edx, row2;
 
-				psraw xmm1 , 3;						// >>3(/8個平均)
-				packuswb xmm1 , xmm1;		//word->byte
-				movq qword ptr[AVGs] , xmm1;		//avrg
+				movq xmm2, qword ptr[eax + ebx];		//i
+				movq xmm3, qword ptr[ecx + ebx];		//p
+				movq xmm4, qword ptr[edx + ebx + 2];	//l
+				movq xmm5, qword ptr[edx + ebx - 2];	//m
+				punpcklbw xmm2, xmm0;		//byte->word
+				punpcklbw xmm3, xmm0;
+				punpcklbw xmm4, xmm0;
+				punpcklbw xmm5, xmm0;
+
+				paddw xmm1, xmm2;					//j+k+n+o +i+p+l+m
+				paddw xmm1, xmm3;
+				paddw xmm1, xmm4;
+				paddw xmm1, xmm5;
+
+				psraw xmm1, 3;						// >>3(/8個平均)
+				packuswb xmm1, xmm1;		//word->byte
+				movq qword ptr[AVGs], xmm1;		//avrg
 
 			// - - - - - - - - - - - - - - - - - - - - - - - - - - -
 			// DIF -- 61
-				
-				movq mm1 , qword ptr[AVGs];
-				pxor mm0 , mm0;
+
+				movq mm1, qword ptr[AVGs];
+				pxor mm0, mm0;
 				//MM2 = PIXEL DE AL LADO 次のピクセル
 
-				movq mm2 , qword ptr[eax+ebx];		//i
-				movq mm3 , mm1;						//avrg
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;					//mm2=max(i,avrg)
-				pminub mm3 , mm4;					//mm3=min(i,avrg)
-				psubusb mm2 , mm3;					// |i-avrg|
-				paddusb   mm0 , mm2;				//mmm0はzeroなので、movと同じ
-				movq mm2 , qword ptr[ecx+ebx];		//p
-				movq mm3 , mm1;						//avrg
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;
-				movq mm2 , qword ptr[edx+ebx+2];	//l
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;
-				movq mm2 , qword ptr[edx+ebx-2];	//m
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;				//|i-avrg|+|p-avrg|+|l-avrg|+|m-avrg|
+				movq mm2, qword ptr[eax + ebx];		//i
+				movq mm3, mm1;						//avrg
+				movq mm4, mm2;
+				pmaxub mm2, mm1;					//mm2=max(i,avrg)
+				pminub mm3, mm4;					//mm3=min(i,avrg)
+				psubusb mm2, mm3;					// |i-avrg|
+				paddusb   mm0, mm2;				//mmm0はzeroなので、movと同じ
+				movq mm2, qword ptr[ecx + ebx];		//p
+				movq mm3, mm1;						//avrg
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;
+				movq mm2, qword ptr[edx + ebx + 2];	//l
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;
+				movq mm2, qword ptr[edx + ebx - 2];	//m
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;				//|i-avrg|+|p-avrg|+|l-avrg|+|m-avrg|
 
-				mov eax , row1;
-				mov ecx , row3;
+				mov eax, row1;
+				mov ecx, row3;
 
-				movq mm2 , qword ptr[eax+ebx-1];	//j
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;
-				movq mm2 , qword ptr[eax+ebx+1];	//k
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;
-				movq mm2 , qword ptr[ecx+ebx-1];	//n
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;
-				movq mm2 , qword ptr[ecx+ebx+1];	//o
-				movq mm3 , mm1;
-				movq mm4 , mm2;
-				pmaxub mm2 , mm1;
-				pminub mm3 , mm4; 
-				psubusb mm2 , mm3;
-				paddusb   mm0 , mm2;				//|i-avrg|+|p-avrg|+|l-avrg|+|m-avrg|+|j-avrg|+|k-avrg|+|n-avrg|+|o-avrg|	0/255でリミット
+				movq mm2, qword ptr[eax + ebx - 1];	//j
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;
+				movq mm2, qword ptr[eax + ebx + 1];	//k
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;
+				movq mm2, qword ptr[ecx + ebx - 1];	//n
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;
+				movq mm2, qword ptr[ecx + ebx + 1];	//o
+				movq mm3, mm1;
+				movq mm4, mm2;
+				pmaxub mm2, mm1;
+				pminub mm3, mm4;
+				psubusb mm2, mm3;
+				paddusb   mm0, mm2;				//|i-avrg|+|p-avrg|+|l-avrg|+|m-avrg|+|j-avrg|+|k-avrg|+|n-avrg|+|o-avrg|	0/255でリミット
 
-				movq qword ptr[DIFs] , mm0;
+				movq qword ptr[DIFs], mm0;
 				emms;
 			}
 			// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 			// FUNCION 44
-			
+
 			__declspec(align(16)) __int16 B[8];
-			for (int i=0;i<8;i++){
-				B[i] = funcion[ DIFs[i]];
+			for (int i = 0; i < 8; i++) {
+				B[i] = funcion[DIFs[i]];
 			}
 
-			if(show)
-				__asm{
-					mov edi , dstp;
-					//movq xmm1 , qword ptr[B];
-					//movhpd xmm1 , qword ptr[B+8];
-					movdqu xmm1,B;
+			if (show)
+				__asm {
+				mov edi, dstp;
+				//movq xmm1 , qword ptr[B];
+				//movhpd xmm1 , qword ptr[B+8];
+				movdqu xmm1, B;
 
-					packuswb xmm1 , xmm1;
-					movq qword ptr[edi+ebx] , xmm1;
-					emms;
-				}
+				packuswb xmm1, xmm1;
+				movq qword ptr[edi + ebx], xmm1;
+				emms;
+			}
 			else
-				__asm{
-					//ahora xmm1 = B	今
-					//      xmm4 = A = B+4			//多分どこかのバージョンからか16ではなく4になってる(下のコメントが正しければ演算中のオーバフロー回避)
-					movddup xmm6 , qword ptr[OCHO];	//[0 4]*4*2 doubleじゃなく整数だけど、出来るからよい
-					//pxor xmm0 , xmm0;
+				__asm {
+				//ahora xmm1 = B	今
+				//      xmm4 = A = B+4			//多分どこかのバージョンからか16ではなく4になってる(下のコメントが正しければ演算中のオーバフロー回避)
+				movddup xmm6, qword ptr[OCHO];	//[0 4]*4*2 doubleじゃなく整数だけど、出来るからよい
+				//pxor xmm0 , xmm0;
 
-					//movq xmm1 , qword ptr[B];
-					//movhpd xmm1 , qword ptr[B+8];
-					movdqu xmm1,B;					//sse2使えば1コマンドになるけど速度はあまり変わらない・・・ ちなみに最近のCPUはalignされてればaでもuでもコストは同じらしい？
+				//movq xmm1 , qword ptr[B];
+				//movhpd xmm1 , qword ptr[B+8];
+				movdqu xmm1, B;					//sse2使えば1コマンドになるけど速度はあまり変わらない・・・ ちなみに最近のCPUはalignされてればaでもuでもコストは同じらしい？
 
-					movaps xmm5 , xmm1;
-					movaps xmm4 , xmm1;
-					paddw  xmm4 , xmm6;				//A=B+ocho
+				movaps xmm5, xmm1;
+				movaps xmm4, xmm1;
+				paddw  xmm4, xmm6;				//A=B+ocho
 
 
-					//PARTE FINAL
-					//xmm1 = (medio)
-					//xmm2 = (AVGs)
-					//xmm4 = A
-					//xmm5 = B
-					mov eax , row2;
-					mov ebx , w;
-					mov edi , dstp;
-					
-					movq xmm1 , qword ptr[eax+ebx];		//X(中央値)
-					movq xmm2 , qword ptr[AVGs];
-					
-					punpcklbw xmm1 , xmm0;
-					punpcklbw xmm2 , xmm0;
-					
-/*
-					// - - - - - -		A=B+4にすればオーバーフローしないからここ要らない、と書いてある もしくはfloatにするか
-				// ESTA PARTE ES EL REMPLAZO , PERO MAS LARGO
-				// PARA EVITAR EL OVERFLOW CON B GRANDE, PERO
-				// SI USO A=B+4 EN LUGAR DE 16, SE EVITA
-				// SINO TENGO QUE USAR ESTOS EN FLOAT
-					movaps xmm7 , xmm1;
-					movaps xmm6 , xmm2;
-					//xmm1 = Medio * A
-					pmullw xmm1, xmm4;
-					pmulhw xmm7, xmm4;
-					//xmm2 = AVG * B
-					pmullw xmm2, xmm5;
+				//PARTE FINAL
+				//xmm1 = (medio)
+				//xmm2 = (AVGs)
+				//xmm4 = A
+				//xmm5 = B
+				mov eax, row2;
+				mov ebx, w;
+				mov edi, dstp;
 
-					pmulhw xmm6, xmm5;
-					
-					movaps xmm0, xmm1;
-					punpcklwd xmm1, xmm7;
-					punpckhwd xmm0, xmm7;
+				movq xmm1, qword ptr[eax + ebx];		//X(中央値)
+				movq xmm2, qword ptr[AVGs];
 
-					movaps xmm4, xmm2;
-					punpcklwd xmm2, xmm6;
-					punpckhwd xmm4, xmm6;
-					//de 32 tengo xmm1,xmm0 y xmm2,xmm4
-					//resto
-					psubd xmm1,xmm2;
-					psubd xmm0,xmm4;
-					//divido
-					psrad xmm1 , 4;
-					psrad xmm0 , 4;
+				punpcklbw xmm1, xmm0;
+				punpcklbw xmm2, xmm0;
 
-					//de int32 a int16
-					packssdw xmm1 , xmm1;
-					packssdw xmm0 , xmm0;
-					//los uno
-					movsd xmm0, xmm1;
-					movaps xmm1, xmm0;
-*/					
-					
-					//xmm1 = Medio * A
-					pmullw xmm1, xmm4;
-					//xmm2 = AVG * B
-					pmullw xmm2, xmm5;				
-					//xmm1 = Z0 *A - AVG *B (con saturacion)	dst=org + funcion[diff] *(org-avr)/ocho = ( org*(ocho + funcion[diff]) - avr*funcion[diff] )	/ocho
-					psubw  xmm1 , xmm2;
-					//divido por 4 (shift 2 en ints 16) y desempaco a 8
-					psraw xmm1 , PRECISION;
-					// - - - - - - - - - - 
+				/*
+									// - - - - - -		A=B+4にすればオーバーフローしないからここ要らない、と書いてある もしくはfloatにするか
+								// ESTA PARTE ES EL REMPLAZO , PERO MAS LARGO
+								// PARA EVITAR EL OVERFLOW CON B GRANDE, PERO
+								// SI USO A=B+4 EN LUGAR DE 16, SE EVITA
+								// SINO TENGO QUE USAR ESTOS EN FLOAT
+									movaps xmm7 , xmm1;
+									movaps xmm6 , xmm2;
+									//xmm1 = Medio * A
+									pmullw xmm1, xmm4;
+									pmulhw xmm7, xmm4;
+									//xmm2 = AVG * B
+									pmullw xmm2, xmm5;
 
-					packuswb xmm1 , xmm1;
-					movq qword ptr[edi+ebx] , xmm1;
-					emms;
-				}
+									pmulhw xmm6, xmm5;
+
+									movaps xmm0, xmm1;
+									punpcklwd xmm1, xmm7;
+									punpckhwd xmm0, xmm7;
+
+									movaps xmm4, xmm2;
+									punpcklwd xmm2, xmm6;
+									punpckhwd xmm4, xmm6;
+									//de 32 tengo xmm1,xmm0 y xmm2,xmm4
+									//resto
+									psubd xmm1,xmm2;
+									psubd xmm0,xmm4;
+									//divido
+									psrad xmm1 , 4;
+									psrad xmm0 , 4;
+
+									//de int32 a int16
+									packssdw xmm1 , xmm1;
+									packssdw xmm0 , xmm0;
+									//los uno
+									movsd xmm0, xmm1;
+									movaps xmm1, xmm0;
+				*/
+
+				//xmm1 = Medio * A
+				pmullw xmm1, xmm4;
+				//xmm2 = AVG * B
+				pmullw xmm2, xmm5;
+				//xmm1 = Z0 *A - AVG *B (con saturacion)	dst=org + funcion[diff] *(org-avr)/ocho = ( org*(ocho + funcion[diff]) - avr*funcion[diff] )	/ocho
+				psubw  xmm1, xmm2;
+				//divido por 4 (shift 2 en ints 16) y desempaco a 8
+				psraw xmm1, PRECISION;
+				// - - - - - - - - - - 
+
+				packuswb xmm1, xmm1;
+				movq qword ptr[edi + ebx], xmm1;
+				emms;
+			}
 
 		}
 		//copio los dos ultimos pixels
-		dstp[rowsize-2]=row2[rowsize-2];			//last-1,last列はソースをコピー
-		dstp[rowsize-1]=row2[rowsize-1];
+		dstp[rowsize - 2] = row2[rowsize - 2];			//last-1,last列はソースをコピー
+		dstp[rowsize - 1] = row2[rowsize - 1];
+
 #endif
 
 		row0=row1;
@@ -402,7 +399,8 @@ void FieldUnSharpY(unsigned char* dstp,const unsigned char* srcp
 		dstp+=dst_pitch;
 	}
 	memcpy((BYTE*) dstp, (BYTE*) row2, rowsize);			//last-1,last行目はソースをコピー
-	memcpy((BYTE*) dstp+dst_pitch, (BYTE*) row3, rowsize);
+	dstp = dstp + dst_pitch;
+	memcpy((BYTE*) dstp, (BYTE*) row3, rowsize);
 }
 
 
